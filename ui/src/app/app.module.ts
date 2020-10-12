fff5e64dd3f61b750cfbce7f6cfa8c003243f2ee
Holm Hinrichs <hinrichs@ponton.de>
2020-10-12 15:45:55 +0200

    DCSA-3 Cards Added

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +10,10 @@ import { VesselComponent } from './vessel/vessel.component';
 import { VesselEditorComponent } from './vessel-editor/vessel-editor.component';
 import { TimestampTableComponent } from './timestamp-table/timestamp-table.component';
 import { TimestampEditorComponent } from './timestamp-editor/timestamp-editor.component';
+
+//primeNG
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -20,7 +26,9 @@ import { TimestampEditorComponent } from './timestamp-editor/timestamp-editor.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
