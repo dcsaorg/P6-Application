e52c3b47f77616b1bf0ea0aad35700d5f2b081db
Paul Bischof <bischof@ponton.de>
2020-10-14 13:33:22 +0200

    DCSA-3 added pojo jooq generation and added post for VesselController

--- backend/src/main/java/org/dcsa/portcall/controller/VesselController.java
+++ backend/src/main/java/org/dcsa/portcall/controller/VesselController.java
@@ -1,7 +1,11 @@
 package org.dcsa.portcall.controller;
 
+import org.dcsa.portcall.db.tables.pojos.Vessel;
+import org.dcsa.portcall.db.tables.records.VesselRecord;
 import org.jooq.DSLContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,6 +24,13 @@ public class VesselController {
     @GetMapping("/vessels")
     public List<String> vessels() {
         return dsl.select(VESSEL.NAME).from(VESSEL).fetch(VESSEL.NAME);
+    }
+
+    @PostMapping("/vessel")
+    public Vessel addVessel(@RequestBody Vessel vessel) {
+        final VesselRecord vesselRecord = dsl.newRecord(VESSEL, vessel);
+        dsl.executeInsert(vesselRecord);
+        return dsl.select().from(VESSEL).where(VESSEL.ID.eq(vesselRecord.getId())).fetchAny().into(Vessel.class);
     }
 
 }
