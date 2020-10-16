400c296b9bef898820810669da3400606a1f6585
Jörg Eichhorn <eichhorn@ponton.de>
2020-10-16 20:37:28 +0200

    DCSA-3 Removed unused test

--- ui/src/app/controller/port-id-to-port.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {PortIdToPortPipe} from './port-id-to-port.pipe';
-
-describe('PortIdToPortPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PortIdToPortPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
