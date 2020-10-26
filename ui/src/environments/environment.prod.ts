9a6fa52d32c7c7bca64932d8d1f0ffa667c2b2c4
Jörg Eichhorn <eichhorn@ponton.de>
2020-10-26 13:01:52 +0100

    DCSA-3 Dynamically setting production url

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true
 };
 
-export const BACKEND_URL = "http://localhost:8080";
+// define Server here, if you are not in production mode and want to have another server than http://localhost:8080
+// for remote server accessing your local gui for e.g. sahi test insert your local rest client here
+export const BACKEND_URL =  window.location.origin + "/api";
