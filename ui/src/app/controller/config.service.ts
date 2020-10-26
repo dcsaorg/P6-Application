bb7cfd554afc4d71d1dc8ec9f93ccb8483e3d9b2
Jörg Eichhorn <eichhorn@ponton.de>
2020-10-26 19:53:25 +0100

    DCSA-3 Changed loading of config.json

--- ui/src/app/controller/config.service.ts
+++ ui/src/app/controller/config.service.ts
@@ -1,16 +1,24 @@
-import {Injectable} from '@angular/core';
-import ConfigJson from '../../assets/config.json';
+import {Injectable, OnInit} from '@angular/core';
 import {Config} from "../model/config";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ConfigService {
+export class ConfigService implements OnInit {
 
-  constructor() {
+  config: Config;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<Config>("assets/config.json").subscribe(data =>{
+      this.config = data;
+    })
   }
 
   getConfig() : Config {
-    return ConfigJson;
+    return this.config;
   }
 }
