ed617b336c4857b999a99e9ffa5a7643b3cb69de
Holm Hinrichs <hinrichs@ponton.de>
2020-10-28 20:56:56 +0100

    DCSA-3 DCSA Timezone support for port of call

---
+++ ui/src/app/controller/timeStampToTimeZone.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Port} from "../model/port";
+import {PortcallTimestamp} from "../model/portcall-timestamp";
+import {PortIdToPortPipe} from "./port-id-to-port.pipe";
+import {toNumbers} from "@angular/compiler-cli/src/diagnostics/typescript_version";
+import {timestamp} from "rxjs/operators";
+import {DatePipe} from "@angular/common";
+
+
+@Pipe({
+  name: 'timestampToTimezone'
+})
+export class TimestampToTimezonePipe implements PipeTransform {
+
+  transform(inputDate :Date, portCallTimestamp: PortcallTimestamp, portlist : Port[]): string {
+    const portId: number = (typeof portCallTimestamp.portOfCall === "number"?portCallTimestamp.portOfCall:portCallTimestamp.portOfCall.id);
+    const timeZone = new PortIdToPortPipe().transform(portId, portlist).timezone;
+    return new DatePipe('en-GB').transform(inputDate, "MM/dd/yyyy HH:mm zzzz", timeZone);
+  }
+
+}
