00e07336b72dc8c1b269e6eba0983f2cf9162c25
Jörg Eichhorn <eichhorn@ponton.de>
2020-10-29 12:45:32 +0100

    DCSA-3 Replaced springfox with springdoc-openapi and enabled spring actuator

--- backend/src/main/java/org/dcsa/portcall/PortCallConfiguration.java
+++ backend/src/main/java/org/dcsa/portcall/PortCallConfiguration.java
@@ -5,23 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @ComponentScan("org.dcsa.portcall")
 public class PortCallConfiguration {
-
-    @Bean
-    public Docket swaggerApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 
     @Bean
     public WebMvcConfigurer configurer() {
