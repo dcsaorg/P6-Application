9259674db14d7b3bab37cbff7043c96e7708f8d3
Holm Hinrichs <hinrichs@ponton.de>
2020-11-02 17:51:11 +0100

    P6-38 Removing Timezone offsets from Tableview

--- ui/src/app/controller/timeStampToTimeZone.pipe.ts
+++ ui/src/app/controller/timeStampToTimeZone.pipe.ts
@@ -15,7 +15,7 @@ export class TimestampToTimezonePipe implements PipeTransform {
   transform(inputDate :Date, portCallTimestamp: PortcallTimestamp, portlist : Port[]): string {
     const portId: number = (typeof portCallTimestamp.portOfCall === "number"?portCallTimestamp.portOfCall:portCallTimestamp.portOfCall.id);
     const timeZone = new PortIdToPortPipe().transform(portId, portlist).timezone;
-    return new DatePipe('en-GB').transform(inputDate, "MM/dd/yyyy HH:mm zzzz", timeZone);
+    return new DatePipe('en-GB').transform(inputDate, "MM/dd/yyyy HH:mm", timeZone);
   }
 
 }
