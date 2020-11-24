98ccee15fa45a351be64bc729e39cd8569b6f816
Jörg Eichhorn <eichhorn@ponton.de>
2020-11-24 20:00:26 +0100

    P6-52 Refactoring

---
+++ backend/src/test/java/org/dcsa/portcall/service/AbstractDatabaseTest.java
@@ -0,0 +1,50 @@
+package org.dcsa.portcall.service;
+
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jooq.JooqTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+@ContextConfiguration(initializers = AbstractDatabaseTest.Initializer.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@JooqTest
+@Testcontainers
+public abstract class AbstractDatabaseTest {
+
+    @Autowired
+    protected DSLContext dsl;
+
+    @Container
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("registry.ponton.de/postgres:10-alpine").asCompatibleSubstituteFor("postgres"))
+            .withDatabaseName("dcsa")
+            .withUsername("dcsa")
+            .withPassword("dcsapw");
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues testPropertyValues = TestPropertyValues.of(Map.of(
+                    "spring.datasource.url", postgres.getJdbcUrl(),
+                    "spring.datasource.username", postgres.getUsername(),
+                    "spring.datasource.password", postgres.getPassword(),
+                    "spring.flyway.url", postgres.getJdbcUrl(),
+                    "spring.flyway.username", postgres.getUsername(),
+                    "spring.flyway.password", postgres.getPassword()
+            ));
+            testPropertyValues.applyTo(configurableApplicationContext);
+        }
+    }
+}
--- backend/src/test/java/org/dcsa/portcall/service/persistence/MessageServiceTest.java
+++ backend/src/test/java/org/dcsa/portcall/service/persistence/MessageServiceTest.java
@@ -2,62 +2,19 @@ package org.dcsa.portcall.service.persistence;
 
 import org.dcsa.portcall.db.enums.MessageDirection;
 import org.dcsa.portcall.db.tables.pojos.Message;
-import org.jooq.DSLContext;
+import org.dcsa.portcall.service.AbstractDatabaseTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jooq.JooqTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-import javax.validation.constraints.NotNull;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.dcsa.portcall.db.Tables.MESSAGE;
 
-@ContextConfiguration(initializers = MessageServiceTest.Initializer.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@JooqTest
-@Testcontainers
-class MessageServiceTest {
+class MessageServiceTest extends AbstractDatabaseTest {
 
     public static final String TEST_MESSAGE_CONTENT = "test";
-
-    @Autowired
-    private DSLContext dsl;
-
-    @Container
-    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-            DockerImageName.parse("registry.ponton.de/postgres:10-alpine").asCompatibleSubstituteFor("postgres"))
-            .withDatabaseName("dcsa")
-            .withUsername("dcsa")
-            .withPassword("dcsapw");
-
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        @Override
-        public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues testPropertyValues = TestPropertyValues.of(Map.of(
-                    "spring.datasource.url", postgres.getJdbcUrl(),
-                    "spring.datasource.username", postgres.getUsername(),
-                    "spring.datasource.password", postgres.getPassword(),
-                    "spring.flyway.url", postgres.getJdbcUrl(),
-                    "spring.flyway.username", postgres.getUsername(),
-                    "spring.flyway.password", postgres.getPassword()
-            ));
-            testPropertyValues.applyTo(configurableApplicationContext);
-        }
-    }
 
     @Test
     void testService() throws Exception {
