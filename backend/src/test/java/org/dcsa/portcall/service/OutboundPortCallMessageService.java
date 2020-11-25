9a78e4f6d12f0fd6e8e48f3395860a6cb194778d
Holm Hinrichs <hinrichs@ponton.de>
2020-11-25 20:39:52 +0100

    P6-60 Added OutboundMessageServiceTest

--- backend/src/test/java/org/dcsa/portcall/service/OutboundPortCallMessageService.java
+++ backend/src/test/java/org/dcsa/portcall/service/OutboundPortCallMessageService.java
@@ -1,16 +1,20 @@
 package org.dcsa.portcall.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.dcsa.portcall.PortCallProperties;
 import org.dcsa.portcall.db.tables.pojos.PortCallTimestamp;
-import org.dcsa.portcall.message.DCSAMessage;
-import org.dcsa.portcall.message.RoleType;
+import org.dcsa.portcall.message.*;
 import org.dcsa.portcall.service.persistence.PortCallTimestampService;
+import org.dcsa.portcall.service.persistence.PortService;
+import org.dcsa.portcall.service.persistence.VesselService;
 import org.junit.jupiter.api.Test;
+import org.postgresql.translation.messages_bg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,47 +40,26 @@ class OutboundPortCallMessageServiceTest extends AbstractDatabaseTest {
 
     private void testExampleMessage(int id) throws Exception {
         this.config.setSenderRole(RoleType.CARRIER);
+        this.config.setSenderIdType(CodeType.SMDG_LINER_CODE);
         this.config.setSenderId("TST");
         PortCallTimestamp timestamp = timestampService.getTimeStampById(id).get();
-        DCSAMessage message = this.outboundService.process(timestamp).get();
+        DCSAMessage<PortCallMessage> message = this.outboundService.process(timestamp).get();
         message.setProcessId("00000000-1111-2222-3333-ABCDEFGHIJKL");
         OffsetDateTime now = OffsetDateTime.of(2020, 11, 25, 18, 48, 0, 0, ZoneOffset.UTC);
         message.setMessageDateTime(now);
-        String msgJson =  this.outboundService.getJsonMapper().writeValueAsString(message);
-        String targetJson = "{\n" +
-                "  \"DCSAMessage\" : {\n" +
-                "    \"MessageDateTime\" : \"2020-11-25T18:48Z\",\n" +
-                "    \"SenderRole\" : \"CARRIER\",\n" +
-                "    \"SenderIdType\" : \"SMDG-LINER-CODE\",\n" +
-                "    \"SenderId\" : \"TST\",\n" +
-                "    \"ReceiverRole\" : \"TERMINAL\",\n" +
-                "    \"ReceiverIdType\" : \"TERMINAL\",\n" +
-                "    \"ReceiverId\" : \"ESALG:CTT\",\n" +
-                "    \"ProcessType\" : \"PortCall\",\n" +
-                "    \"ProcessId\" : \"00000000-1111-2222-3333-ABCDEFGHIJKL\",\n" +
-                "    \"MessageType\" : \"PortCallMessage\",\n" +
-                "    \"Payload\" : {\n" +
-                "      \"VesselIdType\" : \"IMO-VESSEL-NUMBER\",\n" +
-                "      \"VesselId\" : \"1234560\",\n" +
-                "      \"PortIdType\" : \"UN/LOCODE\",\n" +
-                "      \"PortId\" : \"ESALG\",\n" +
-                "      \"TerminalIdType\" : \"TERMINAL\",\n" +
-                "      \"TerminalId\" : \"CTT\",\n" +
-                "      \"NextPortOfCall\" : \"GBFXT\",\n" +
-                "      \"VoyageNumber\" : \"Example Loop 1\",\n" +
-                "      \"Event\" : {\n" +
-                "        \"EventClassifierCode\" : \"EST\",\n" +
-                "        \"TransportEventTypeCode\" : \"ARRI\",\n" +
-                "        \"LocationId\" : \"urn:mrn:ipcdmc:location:ESALG:berth:CTT:Bollard 55-70\",\n" +
-                "        \"EventDateTime\" : \"2020-11-05T14:00Z\",\n" +
-                "        \"LocationType\" : \"BERTH\"\n" +
-                "      },\n" +
-                "      \"PreviousPortOfCall\" : \"DEHAM\"\n" +
-                "    }\n" +
-                "  }\n" +
-                "}";
 
-        assertThat(targetJson).isEqualTo(msgJson);
+
+        assertThat(message.getSenderRole()).isEqualTo(RoleType.CARRIER);
+        assertThat(message.getSenderId()).isEqualTo("TST");
+        assertThat(message.getSenderIdType()).isEqualTo(CodeType.SMDG_LINER_CODE);
+        assertThat(message.getMessageDateTime()).isEqualTo( OffsetDateTime.of(2020, 11, 25, 18, 48, 0, 0, ZoneOffset.UTC));
+        assertThat(message.getReceiverRole()).isEqualTo(RoleType.TERMINAL);
+        assertThat(message.getReceiverIdType()).isEqualTo(CodeType.TERMINAL);
+        assertThat(message.getPayload().getVesselIdType()).isEqualTo(CodeType.IMO_VESSEL_NUMBER);
+        assertThat(message.getPayload().getEvent().getEventClassifierCode()).isEqualTo(EventClassifierCode.EST);
+        assertThat(message.getPayload().getEvent().getTransportEventTypeCode()).isEqualTo(TransportEventType.ARRI);
+        assertThat(message.getPayload().getEvent().getLocationType()).isEqualTo(LocationType.BERTH);
+
     }
 
 }
