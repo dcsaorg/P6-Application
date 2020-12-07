26e10b0bea3a9a04f7459d1a35e5cd9b3d7cc751
Jörg Eichhorn <eichhorn@ponton.de>
2020-12-07 22:45:42 +0100

    P6-83 Changed initialization of testcontainers

--- backend/src/test/java/org/dcsa/portcall/service/AbstractDatabaseTest.java
+++ backend/src/test/java/org/dcsa/portcall/service/AbstractDatabaseTest.java
@@ -1,51 +1,45 @@
 package org.dcsa.portcall.service;
 
 import org.jooq.DSLContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import javax.validation.constraints.NotNull;
-import java.util.Map;
-
-@ContextConfiguration(initializers = AbstractDatabaseTest.Initializer.class)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
 public abstract class AbstractDatabaseTest {
 
+    private static final Logger log = LoggerFactory.getLogger(AbstractDatabaseTest.class);
+
     @Autowired
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     protected DSLContext dsl;
 
-    @Container
     private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
             DockerImageName.parse("registry.ponton.de/postgres:10-alpine").asCompatibleSubstituteFor("postgres"))
             .withDatabaseName("dcsa")
             .withUsername("dcsa")
-            .withPassword("dcsapw");
+            .withPassword("dcsapw").withLogConsumer(new Slf4jLogConsumer(log))
+            .waitingFor(
+                    Wait.forLogMessage(".*database system is ready to accept connections.*\\n", 1)
+            );
 
-    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static {
+        postgres.start();
+    }
 
-        @Override
-        @SuppressWarnings("NullableProblems")
-        public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues testPropertyValues = TestPropertyValues.of(Map.of(
-                    "spring.datasource.url", postgres.getJdbcUrl(),
-                    "spring.datasource.username", postgres.getUsername(),
-                    "spring.datasource.password", postgres.getPassword(),
-                    "spring.datasource.hikari.maxLifetime", "600000",
-                    "spring.flyway.url", postgres.getJdbcUrl(),
-                    "spring.flyway.username", postgres.getUsername(),
-                    "spring.flyway.password", postgres.getPassword()
-            ));
-            testPropertyValues.applyTo(configurableApplicationContext);
-        }
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.flyway.url", postgres::getJdbcUrl);
+        registry.add("spring.flyway.username", postgres::getUsername);
+        registry.add("spring.flyway.password", postgres::getPassword);
     }
 }
