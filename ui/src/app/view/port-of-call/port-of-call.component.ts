9fe3063d8bc04c39410ee2e2b3d6d531ad7e70ad
holmHin <holm.hinrichs@t-online.de>
2021-03-16 20:58:11 +0100

    P6-138 Filtering Terminals on selected Port UNLocode

--- ui/src/app/view/port-of-call/port-of-call.component.ts
+++ ui/src/app/view/port-of-call/port-of-call.component.ts
@@ -2,7 +2,6 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Port} from "../../model/base/port";
 import {SelectItem} from "primeng/api";
 import {PortService} from "../../controller/services/base/port.service";
-import {translate} from "@angular/localize/src/translate";
 import {LangChangeEvent, TranslateService} from "@ngx-translate/core";
 import {Globals} from "../globals";
 import {TerminalService} from "../../controller/services/base/terminal.service";
@@ -37,26 +36,35 @@ export class PortOfCallComponent implements OnInit {
     });
   }
 
-  selectPortOfCall = () => this.portOfCallNotifier.emit(this.portOfCall);
-  selectTerminal = () => {}
+  selectPortOfCall = () => {
+    this.updateTerminalOptions();
+    this.portOfCallNotifier.emit(this.portOfCall);
+  };
+  selectTerminal = () => {
+  }
 
-  updateTerminalOptions(){
+  updateTerminalOptions() {
     this.terminalService.getTerminals().subscribe(terminals => {
       this.globals.terminals = terminals;
       this.terminalOptions = [];
-      this.terminalOptions.push({ label: this.translate.instant('general.terminal.select'), value: null });
-      terminals.forEach((terminal => this.terminalOptions.push({label: terminal.smdgCode, value: terminal})))
+      this.terminalOptions.push({label: this.translate.instant('general.terminal.select'), value: null});
+      terminals.forEach(terminal => {
+        if (terminal.port == this.portOfCall.id) {
+          this.terminalOptions.push({label: terminal.smdgCode, value: terminal})
+        }
+      });
     })
   }
 
   updatePortOfcallOptions() {
     this.portService.getPorts().subscribe(ports => {
-      this.globals.ports  = ports;
+      this.globals.ports = ports;
       this.portOptions = [];
-      this.portOptions.push({ label: this.translate.instant('general.port.select'), value: null });
+      this.portOptions.push({label: this.translate.instant('general.port.select'), value: null});
       ports.forEach(port => {
         this.portOptions.push({label: port.unLocode, value: port});
       });
+
     });
   }
 
