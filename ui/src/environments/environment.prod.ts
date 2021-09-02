f65f6fe1e57f4d3a9d20826e765cb14a3d95a83f
abf <abf@asseco.dk>
2021-09-02 11:25:55 +0200

    Update environment.prod.ts

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  cognitoUserPoolId: "eu-west-1_rJGpA7BVH",
+  cognitoAppClientId: "3bdgnq3ihck83j6pkl36ccdker" 
 };
 
 // define Server here, if you are not in production mode and want to have another server than http://localhost:8080
