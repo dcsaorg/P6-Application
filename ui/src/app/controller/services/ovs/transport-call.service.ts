1cb0d857b7be488f50d7aa3cdfe735939195c899
Andreas <ah@asseco.dk>
2021-09-03 15:42:43 +0200

    Implement ETA functionality to ´TransportCall´ table

--- ui/src/app/controller/services/ovs/transport-call.service.ts
+++ ui/src/app/controller/services/ovs/transport-call.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {BACKEND_URL} from "../../../../environments/environment";
-import {Observable} from "rxjs";
-import {TransportCall} from "../../../model/ovs/transport-call";
-import {map} from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { BACKEND_URL } from "../../../../environments/environment";
+import { from, Observable } from "rxjs";
+import { TransportCall } from "../../../model/ovs/transport-call";
+import { map, mergeMap, toArray, concatMap } from "rxjs/operators";
 import { Timestamp } from 'src/app/model/ovs/timestamp';
 
 @Injectable({
@@ -20,8 +20,27 @@ export class TransportCallService {
   }
 
   getTransportCalls = (): Observable<TransportCall[]> =>
-    this.httpClient.get<TransportCall[]>(this.TRANSPORT_CALL_URL).pipe(map(transportCalls => this.postProcess(transportCalls)));
-  
+    this.httpClient.get<TransportCall[]>(this.TRANSPORT_CALL_URL).pipe(
+      mergeMap((transportCalls => {
+        return from(transportCalls).pipe(
+          map(this.extractVesselAttributes),
+          concatMap((transportCall) =>
+            this.getOperationsEventstoTimestamp(transportCall.transportCallID).pipe(map(timestamps => {
+              if(timestamps.length > 0){
+                let timestamp = timestamps[0]
+                transportCall.estimatedDateofArrival = timestamp['eventDateTime'];
+              } else  {
+                transportCall.estimatedDateofArrival = 'N/A';
+              }
+              return transportCall;
+            }))
+          ),
+          toArray()
+        )
+        
+      }))
+    );
+
   getOperationsEventstoTimestamp = (transportCallId: string): Observable<Timestamp[]> =>
   this.httpClient.get<Timestamp[]>(this.OPERATIONS_EVENT_URL+ "&transportCallID=" + transportCallId).pipe(map(transportCalls => (transportCalls)));
 
@@ -30,18 +49,6 @@ export class TransportCallService {
   addTransportCall = (transportCall: TransportCall): Observable<TransportCall> =>
     this.httpClient.post<TransportCall>(this.TRANSPORT_CALL_URL, transportCall)
 
-
-  /**
-   * Function that will process the retrieved transportCalls in order to add som additional information
-   */
-  private postProcess(transportCalls: TransportCall[]):TransportCall[]{
-    for(let transportCall of transportCalls){
-
-      transportCall = this.extractVesselAttributes(transportCall)
-      transportCall = this.extractEstimatedDateofArrival(transportCall);
-    }
-    return transportCalls;
-  }
 
   private extractVesselAttributes(transportCall: TransportCall){
    if (transportCall['vessel'] === null){
@@ -53,19 +60,5 @@ export class TransportCallService {
    transportCall.vesselIMONumber = transportCall['vessel']['vesselIMONumber'];
     }
       return transportCall
-
   }
-  private extractEstimatedDateofArrival(transportCall: TransportCall){
-    
-    this.getOperationsEventstoTimestamp(transportCall.transportCallID).subscribe(timestamps => {
-      if(timestamps.length > 0){
-        let timestamp = timestamps[0]
-    
-        transportCall.estimatedDateofArrival = timestamp['eventDateTime'] ;
-      } else  {
-        transportCall.estimatedDateofArrival = 'N/A';
-      }
-    });
-       return transportCall
-   }
 }
