24653ca7803917b5666b7835da2d1bb5a9f43c99
Andreas <ah@asseco.dk>
2021-09-14 18:08:19 +0200

    fixed support for IANA timezones

---
+++ ui/src/app/controller/pipes/moment-date-pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import moment from 'moment-timezone'
+
+
+/*** A moment timezone pipe to support parsing based on time zone abbreviations
+ * covers all cases of offset variation due to daylight saving.
+ *
+ * Same API as DatePipe with additional timezone abbreviation support
+ * Official date pipe dropped support for abbreviations names from Angular V5
+ */
+
+@Pipe({ name: 'momentDate' })
+export class MomentDatePipe extends DatePipe implements PipeTransform {
+  transform(
+    value: string | Date,
+    format: string = 'mediumDate',
+    timezone: string = 'Europe/Prague'
+  ): string {
+    const timezoneOffset = timezone.startsWith("UTC") ? timezone : moment(value).tz(timezone).format('Z');
+    return super.transform(value, format, timezoneOffset);
+  }
+}
