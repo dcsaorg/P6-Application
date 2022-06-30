e50bf82a93f8bf0c33240331d0df9149536bb101
abf <abf@asseco.dk>
2022-06-30 15:41:42 +0200

    Extend OperationsEventTypeCode with new enums

--- src/app/model/enums/operationsEventTypeCode.ts
+++ src/app/model/enums/operationsEventTypeCode.ts
@@ -1,6 +1,8 @@
 export enum OperationsEventTypeCode {
-    STRT  = "STRT",
-    CMPL = "CMPL",
-    ARRI = "ARRI",
-    DEPA = "DEPA"
-  }
+  STRT = "STRT",
+  CMPL = "CMPL",
+  ARRI = "ARRI",
+  DEPA = "DEPA",
+  CANC = "CANC",
+  OMIT = "OMIT"
+}
