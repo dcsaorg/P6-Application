039d3416aa6f476849826fb3a2fd02d5b7757b2a
Niels Thykier <nt@asseco.dk>
2022-08-19 14:30:04 +0200

    Correct the port visit level filters
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/app/controller/services/base/transport-call-filter.service.ts
+++ src/app/controller/services/base/transport-call-filter.service.ts
@@ -1,16 +1,11 @@
 import { Injectable } from "@angular/core";
 import { ReplaySubject } from "rxjs";
 import { Port } from "src/app/model/portCall/port";
-import { Terminal } from "src/app/model/portCall/terminal";
 import {Vessel} from "../../../model/portCall/vessel";
 
 @Injectable() export class TransportCallFilterService {
     private portDataSource = new ReplaySubject<Port>()
     portObservable = this.portDataSource.asObservable()
-
-    private terminalDataSource = new ReplaySubject<Terminal>()
-    terminalObservable = this.terminalDataSource.asObservable()
-
 
     private vesselDataSource = new ReplaySubject<Vessel>()
     vesselObservable = this.vesselDataSource.asObservable()
@@ -19,10 +14,6 @@ import {Vessel} from "../../../model/portCall/vessel";
 
     updatePortFilter(port: Port){
         this.portDataSource.next(port)
-    }
-
-    updateTerminalFilter(terminal: Terminal){
-        this.terminalDataSource.next(terminal)
     }
 
     updateVesselFilter(vessel: Vessel) {
