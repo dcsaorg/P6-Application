6440c760f2ece2a403137fc42dfc1d7cb12a261e
abf <abf@asseco.dk>
2022-08-31 09:35:38 +0200

    DDT-1204: backend filtering NegotiationCycle
    - and minor lang change

--- src/app/controller/services/base/timestamp-definition.service.ts
+++ src/app/controller/services/base/timestamp-definition.service.ts
@@ -1,9 +1,10 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable, of} from "rxjs";
-import {Globals} from "../../../model/portCall/globals";
-import {TimestampDefinitionTO} from "../../../model/jit/timestamp-definition";
-import {map} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { Globals } from "../../../model/portCall/globals";
+import { TimestampDefinitionTO } from "../../../model/jit/timestamp-definition";
+import { map } from 'rxjs/operators';
+import { NegotiationCycle } from 'src/app/model/portCall/negotiation-cycle';
 
 function asMap(timestampDefinitions: TimestampDefinitionTO[]): Map<string, TimestampDefinitionTO> {
   let map = new Map<string, TimestampDefinitionTO>()
@@ -20,9 +21,10 @@ export class TimestampDefinitionService {
   private readonly TIMESTAMP_DEFINITION_BACKEND: string;
   // the timestamps are not likely to change during a work session.
   private definitionCache: TimestampDefinitionTO[] = [];
+  private negotiationCyclesCache: NegotiationCycle[] = [];
 
   constructor(private httpClient: HttpClient,
-              private globals: Globals) {
+    private globals: Globals) {
     this.TIMESTAMP_DEFINITION_BACKEND = globals.config.uiSupportBackendURL + '/unofficial/timestamp-definitions';
   }
 
@@ -30,7 +32,7 @@ export class TimestampDefinitionService {
     if (this.definitionCache.length == 0) {
       let httpParams = new HttpParams();
       httpParams = httpParams.set("canonicalTimestampDefinition", "NULL").set("limit", "250");
-      return this.httpClient.get<TimestampDefinitionTO[]>(this.TIMESTAMP_DEFINITION_BACKEND, {params: httpParams})
+      return this.httpClient.get<TimestampDefinitionTO[]>(this.TIMESTAMP_DEFINITION_BACKEND, { params: httpParams })
         .pipe(
           map(definitions => {
             let table = asMap(definitions);
@@ -52,6 +54,25 @@ export class TimestampDefinitionService {
   }
 
 
+  getNegotiationCycles(): Observable<NegotiationCycle[]> {
+    if (this.negotiationCyclesCache.length == 0) {
+      return this.getTimestampDefinitions().pipe(
+        map(timestampDefinitions => {
+          let uniqueNegotiationCycles: NegotiationCycle[] = [];
+          timestampDefinitions.forEach(timestampDefinitionTO => {
+            if (uniqueNegotiationCycles.find(ng => ng.cycleKey === timestampDefinitionTO.negotiationCycle.cycleKey)) {
+              return;
+            } else {
+              uniqueNegotiationCycles.push(timestampDefinitionTO.negotiationCycle)
+            }
+          })
+          return uniqueNegotiationCycles;
+        }),
+        map(uniqueNegotiationCycles => this.negotiationCyclesCache = uniqueNegotiationCycles)
+      )
+    }
+    return of(this.negotiationCyclesCache);
+  }
 
   getTimestampDefinitionsMap(): Observable<Map<string, TimestampDefinitionTO>> {
     return this.getTimestampDefinitions().pipe(
