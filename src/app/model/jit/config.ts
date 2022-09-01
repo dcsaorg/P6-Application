1593a4c61067ae89e07bb6441c8525ad68bed8f1
Niels Thykier <nt@asseco.dk>
2022-09-01 09:21:01 +0200

    Type fix identifyingCodes
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/app/model/jit/config.ts
+++ src/app/model/jit/config.ts
@@ -1,5 +1,5 @@
 import { PublisherRole } from "../enums/publisherRole";
-import { identifyingCodes } from "../portCall/identifyingCodes";
+import { IdentifyingCode } from "../portCall/identifyingCode";
 import { Publisher } from "../publisher";
 
 export interface Config {
@@ -7,7 +7,7 @@ export interface Config {
   company: string;
   publisherRoles: PublisherRole[];
   publisher: Publisher;
-  identifyingCodes: identifyingCodes;
+  identifyingCodes: IdentifyingCode[];
   uiSupportBackendURL: string;
   jitBackendURL: string;
   dateTimeFormat: string;
