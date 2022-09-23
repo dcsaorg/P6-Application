b734429eb3dcc5a0c96de1e5694343c9ab650677
Niels Thykier <nt@asseco.dk>
2022-09-23 15:04:07 +0200

    Correct bug in VesselTooltipPipe
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/app/controller/pipes/vessel-tooltip.pipe.ts
+++ src/app/controller/pipes/vessel-tooltip.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DimensionUnit } from 'src/app/model/enums/dimensionUnit';
-import { Vessel } from "../../model/portCall/vessel";
+import { Vessel } from '../../model/portCall/vessel';
 
 
 @Pipe({
@@ -9,19 +9,19 @@ import { Vessel } from "../../model/portCall/vessel";
 export class VesselTooltipPipe implements PipeTransform {
 
   transform(vessel: Vessel, milesToDestinationPort: string, vesselDraft: string, vesselDraftUnit?: DimensionUnit): string {
-    let tooltip = "";
-    let unit = vessel?.dimensionUnit; 
-    if(vesselDraftUnit) unit = vesselDraftUnit; 
-    if (milesToDestinationPort) tooltip += `milesToDestinationPort: ${milesToDestinationPort} \n`;
-    if (vesselDraft) tooltip += `vesselDraft: ${vesselDraft} ${vessel?.dimensionUnit} \n `;
+    let tooltip = '';
+    let unit = vessel?.dimensionUnit;
+    if (vesselDraftUnit) { unit = vesselDraftUnit; }
+    if (milesToDestinationPort) { tooltip += `milesToDestinationPort: ${milesToDestinationPort} \n`; }
+    if (vesselDraft) { tooltip += `vesselDraft: ${vesselDraft} ${unit} \n `; }
     if (vessel) {
-      if (vessel.type) tooltip += `Type: ${vessel.type}\n`;
-      if (vessel.width) tooltip += `Width: ${vessel.width} ${vessel.dimensionUnit}\n`;
-      if (vessel.length) tooltip += `Length: ${vessel.length} ${vessel.dimensionUnit}\n`;
-      if (vessel.vesselCallSignNumber) tooltip += `Call sign: ${vessel.vesselCallSignNumber}\n`;
+      if (vessel.type) { tooltip += `Type: ${vessel.type}\n`; }
+      if (vessel.width) { tooltip += `Width: ${vessel.width} ${vessel.dimensionUnit}\n`; }
+      if (vessel.length) { tooltip += `Length: ${vessel.length} ${vessel.dimensionUnit}\n`; }
+      if (vessel.vesselCallSignNumber) { tooltip += `Call sign: ${vessel.vesselCallSignNumber}\n`; }
     }
-    if (tooltip === "") {
-      tooltip = "N/A";
+    if (tooltip === '') {
+      tooltip = 'N/A';
     }
     return tooltip;
   }
