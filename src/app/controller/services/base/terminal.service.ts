a06c26c9d3b924b6989989dbc3285e0a9354e796
abf <abf@asseco.dk>
2022-09-26 10:37:51 +0200

    DDT-1245: add port level filter to terminals

--- src/app/controller/services/base/terminal.service.ts
+++ src/app/controller/services/base/terminal.service.ts
@@ -1,8 +1,9 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Terminal} from "../../../model/portCall/terminal";
-import {Observable} from "rxjs";
-import {Globals} from "../../../model/portCall/globals";
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Terminal } from "../../../model/portCall/terminal";
+import { map, Observable } from "rxjs";
+import { Globals } from "../../../model/portCall/globals";
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,24 @@ export class TerminalService {
   private readonly TERMINAL_URL_BACKEND: string;
 
   constructor(private httpClient: HttpClient,
-              private globals: Globals) {
+    private globals: Globals,
+    private translate: TranslateService) {
     this.TERMINAL_URL_BACKEND = globals.config.uiSupportBackendURL + '/unofficial/terminals?facilitySMDGCode:neq=NULL';
   }
 
-  getTerminalsByUNLocationCode = (UNLocationCode?: string): Observable<Terminal[]> =>
-    this.httpClient.get<Terminal[]>(this.TERMINAL_URL_BACKEND + "&UNLocationCode=" + UNLocationCode);
+  getTerminalsByUNLocationCode(UNLocationCode?: string): Observable<Terminal[]> {
+    return this.httpClient.get<Terminal[]>(this.TERMINAL_URL_BACKEND + "&UNLocationCode=" + UNLocationCode).
+      pipe(map(terminals => this.addPortLevelFilter(terminals)))
+  }
 
+  private addPortLevelFilter(terminals: Terminal[]): Terminal[] {
+    const PORT_LEVEL_FILTER: Terminal = {
+      facilityName: this.translate.instant('general.terminal.portLevelFilter'),
+      facilityBICCode: "NULL",
+      facilitySMDGCode: "NULL",
+      UNLocationCode: "N/A",
+    };
+    terminals.push(PORT_LEVEL_FILTER)
+    return terminals;
+  }
 }
